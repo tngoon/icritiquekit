e71a13976f2eb010c908c1c369f04d1570522eca
Leo (Shih-Chuan) Ku <leo.sc.ku@gmail.com>
2017-12-03 21:43:02 -0800

    Attempt at importing JSON file

--- public/js/TALanding.js
+++ public/js/TALanding.js
@@ -7,4 +7,41 @@ $(document).ready(function() {
 
 function submitID() {
   var ID = $("#TAID").val();
+
+  $.getJSON("../json/StudentList.JSON", function(studnetList)){
+    var col = [];
+    for (var i = 0; i < studentList.length; i++) {
+      for (var key in studentList[i]) {
+        if (col.indexOf(key) === -1) {
+          col.push(key);
+        }
+      }
+    }
+
+    var table = document.createElement("table");
+
+    var tr = table.insertRow(-1);
+
+    for (var i = 0; i < col.length; i++) {
+      var th = document.createElement("th"); // TABLE HEADER.
+      th.innerHTML = col[i];
+      tr.appendChild(th);
+    }
+
+    for (var i = 0; i < studentList.length; i++) {
+
+      tr = table.insertRow(-1);
+
+      for (var j = 0; j < col.length; j++) {
+        var tabCell = tr.insertCell(-1);
+        tabCell.innerHTML = studentList[i][col[j]];
+      }
+    }
+
+    var divContainer = document.getElementById("showTable2");
+    divContainer.innerHTML = "";
+    divContainer.appendChild(table);
+  }
+
+
 }
