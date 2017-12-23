8ee94176cb548425108b04ae3c5f0ed206751452
Luis Calona <lcalona@ucsd.edu>
2017-12-22 20:31:02 -0800

    Saves submissions with correct version

--- save-submissions.js
+++ save-submissions.js
@@ -1,36 +1,52 @@
 var formidable = require('formidable');
 var fs = require('fs');
 
-var quarterDir = "/public/ixd-w18";
 var assignmentNum = "A1";
+var quarterDir = "/public/ixd-w18";
 var assignmentDir = "/assignment-" + assignmentNum;
-var uploadDir = __dirname + quarterDir + assignmentDir + "/";
+var submissionDir = __dirname + quarterDir + assignmentDir + "/";
 
 module.exports = {
 	processForm: function (req, res) {
 		var form = new formidable.IncomingForm({
-			uploadDir: __dirname + quarterDir + "/",
+			uploadDir: submissionDir,
 			keepExtensions: "true"
 		});
 
-		console.log(form);
-
 		form.on('error', function (err) { console.log(err); });
 		form.on('aborted', function () { console.log('Aborted'); });
-		form.on('file', function (name, file) { console.log('file=' + file); });
 
-		form.parse(req, function (err, fields, files) {
-			var oldpath = files.assignment.path;
-			var newpath = __dirname + quarterDir + "/" + fields.pid + ".pdf";
-			fs.rename(oldpath, newpath, function (err) {
-				if (err) throw err;
-			});
-			console.log(files);
-			console.log(fields);
-			console.log("PID: " + fields.pid);
-			console.log(files.assignment);
-		})
+		form.parse(req, saveVersion);
 
 		res.write('File uploaded and moved!');
+	}
+}
+
+// Saves the file with the correct version number
+// * Only works correctly if previous versions are not deleted
+function saveVersion(err, fields, files) {
+	var versionNumber = 0;
+	var extension = ".pdf";
+	var oldpath = files.assignment.path;
+	var newpath;
+
+	// Determine the correct version number for submission
+	do {
+		versionNumber++;
+		newpath = submissionDir + fields.pid + "-" + versionNumber + extension;
+	} while (fsExistsSync(newpath));
+
+	fs.rename(oldpath, newpath, function (err) {
+		if (err) throw err;
+	});
+}
+
+// Returns true if the path exists and false otherwise
+function fsExistsSync(path) {
+	try {
+		fs.accessSync(path);
+		return true;
+	} catch (error) {
+		return false;
 	}
 }
