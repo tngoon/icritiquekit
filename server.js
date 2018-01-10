56ac1f401dfce5cf9306ae636f876bb69eb11934
asweingarten <asweingarten@gmail.com>
2018-01-10 12:41:10 -0800

    File submission test
    - include express middleware "multer" to handle multipart form data
    (which is the "enctype" for file uploads)
      - included in the package.json now
      - added "/submit-assignment" endpoint that accepts POST requests with
      files
    - added submit-assignment-test.html where the functionality can be
    tested

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const PORT = process.env.PORT || 8080;
 const path = require('path');
 
 const INDEX = path.join(__dirname, '/public');
-const server = express()
+const app = express();
+const server = app
 		.all('/', function(req, res, next) {
 	    	res.header("Access-Control-Allow-Origin", "*");
 	    	res.header("Access-Control-Allow-Headers", "X-Requested-With");
@@ -13,5 +14,22 @@ const server = express()
 		})
 		.use(express.static(__dirname + '/public'))
 		.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const multer  = require('multer');
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '/tmp/my-uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
+})
+const upload = multer({ storage });
+
+app.post('/submit-assignment', upload.single('my-assigment'), (req, res, next) => {
+  console.log('hit submit-assignment endpoint');
+  res.end();
+})
+
 
 require('./websocket-logging')(server);
